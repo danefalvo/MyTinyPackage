684c475202c3bb167bc2725289f74db5a4ce3241
Dane Falvo <danefalvo@gmail.com>
2021-04-05 21:12:36 -0700

    Yep yep yep

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyTinyPackage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("OMFG");
+            Console.WriteLine("OMFG!");
         }
     }
 }
